40a2edeec023f46e6fff49da97e373800d1e5f99
torres-sam <torres.sam@tamu.edu>
2025-11-19 12:43:44 -0800

    Added icons

--- astro/src/components/data/projects.js
+++ astro/src/components/data/projects.js
@@ -1,3 +1,10 @@
+import spinnakerVisual from '../../content/spinnaker.svg?raw'
+import kanmVisual from '../../content/radio.svg?raw'
+import pobVisual from '../../content/pob.svg?raw'
+import bitjockeyVisual from '../../content/bitjockey.svg?raw'
+import isabellaVisual from '../../content/isabella.svg?raw'
+import mechanixVisual from '../../content/mechanix.svg?raw'
+
 
 
 export const projects = [
@@ -5,31 +12,41 @@ export const projects = [
     title: "spinnaker",
     description: "Product improvements for Netflix's CI/CD system",
     link: "/projects/spinnaker",
+    visual: spinnakerVisual
+
   },
   {
     title: "kanm",
     description: "Website redesign for KANM Student Radio",
     link: "/projects/kanm",
-    video: "/videos/kanm2.mp4"
+    visual: kanmVisual
+
   },
   {
     title: "bitjockey",
     description: "Open source DJ automation software",
     link: "/projects/bitjockey",
+    visual: bitjockeyVisual
+
   },
   {
     title: "mechanix",
     description: "Research for a sketch recognition physics teaching tool",
     link: "/projects/mechanix",
+    visual: mechanixVisual
+
   },
   {
     title: "isabella",
     description: "My personal media homelab",
     link: "/projects/isabella",
+    visual: isabellaVisual
+
   },
   {
     title: "pob phoenix",
     description: "Personnel cataloging tool built for offshore operations",
-    link: "/projects/phoenix"
+    link: "/projects/phoenix",
+    visual: pobVisual
   }
 ];
