2ba9359d4f78cc169909aad30261871af0237ff1
Sam Torres <samtorres@Sams-MacBook-Air.local>
2025-12-04 09:22:30 -0800

    New page  deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), react()],
+	site: 'https://samuel-torres-code.github.io',
 });
